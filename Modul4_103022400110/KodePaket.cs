f1d37532ff3a9c20d0e46b54459eab247df12edd
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-03-28 08:06:12 +0700

    second commit

---
+++ Modul4_103022400110/KodePaket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class KodePaket
+{
+    // Dictionary untuk menyimpan nama paket dan kode paket
+    private Dictionary<string, string> kodePaket = new Dictionary<string, string>()
+    {
+        { "Basic", "P201" },
+        { "Standard", "P202" },
+        { "Premium", "P203" },
+        { "Unlimited", "P204" },
+        { "Gaming", "P205" },
+        { "Streaming", "P206" },
+        { "Family", "P207" },
+        { "Business", "P208" },
+        { "Student", "P209" },
+        { "Traveler", "P210" },
+    };
+
+    // Method return kode paket berdasarkan nama paket
+    public string getKodePaket(string namaPaket)
+    {
+        return kodePaket[namaPaket];
+    }
+}
--- Modul4_103022400110/Program.cs
+++ Modul4_103022400110/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat objek KodePaket
+        KodePaket kode = new KodePaket();
+        Console.WriteLine("Masukkan nama paket (Basic, Standard, Premium, Unlimited, Gaming, Streaming, Family, Business, Student, Traveler):");
+        string namaPaket = Console.ReadLine();
+
+        // Get KodePaket berdasarkan nama paket
+        string kodepaket = kode.getKodePaket(namaPaket);
+
+        Console.WriteLine("Kode paket untuk " + namaPaket + " adalah: " + kodepaket);
+    }
+}
