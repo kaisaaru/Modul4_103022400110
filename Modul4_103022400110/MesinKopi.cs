54d1948408f5f4e030138dda2254d7c7b7f985de
Kaisar Rayfa Al Baihaqqi <ryf.kaisaaru@gmail.com>
2026-03-28 08:26:21 +0700

    menambahkan class MesinKopi

---
+++ Modul4_103022400110/MesinKopi.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class MesinKopi
+{
+    // Enum untuk mendefinisikan state mesin kopi
+    public enum State
+    {
+        OFF,
+        STANDBY,
+        BREWING,
+        MAINTENANCE
+    }
+
+    // Variabel untuk menyimpan state saat ini
+    private State currentState;
+
+    // Konstruktor untuk inisialisasi state awal
+    public MesinKopi()
+    {
+        currentState = State.OFF;
+        Console.WriteLine("State awal mesin: OFF");
+    }
+
+    // Method untuk mengubah state mesin kopi
+    public void PowerOn()
+    {
+        if (currentState == State.OFF)
+        {
+            currentState = State.STANDBY;
+            Console.WriteLine("Mesin Off berubah menjadi Standby");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    public void StartBrew()
+    {
+        if (currentState == State.STANDBY)
+        {
+            currentState = State.BREWING;
+            Console.WriteLine("Mesin Standby berubah menjadi Brewing");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    public void FinishBrew()
+    {
+        if (currentState == State.BREWING)
+        {
+            currentState = State.STANDBY;
+            Console.WriteLine("Mesin Brewing berubah menjadi Standby");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    public void StartMaintenance()
+    {
+        if (currentState == State.STANDBY)
+        {
+            currentState = State.MAINTENANCE;
+            Console.WriteLine("Mesin Standby berubah menjadi Maintenance");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    public void FinishMaintenance()
+    {
+        if (currentState == State.MAINTENANCE)
+        {
+            currentState = State.STANDBY;
+            Console.WriteLine("Mesin Maintenance berubah menjadi Standby");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    public void PowerOff()
+    {
+        if (currentState == State.STANDBY)
+        {
+            currentState = State.OFF;
+            Console.WriteLine("Mesin Standby berubah menjadi Off");
+        }
+        else
+        {
+            Console.WriteLine("Perubahan state tidak valid");
+        }
+    }
+
+    // Method untuk menampilkan state saat ini
+    public void TampilkanState()
+    {
+        Console.WriteLine("State saat ini: " + currentState);
+    }
+
+}
--- Modul4_103022400110/Program.cs
+++ Modul4_103022400110/Program.cs
@@ -6,12 +6,47 @@ class Program
     {
         // Membuat objek KodePaket
         KodePaket kode = new KodePaket();
-        Console.WriteLine("Masukkan nama paket (Basic, Standard, Premium, Unlimited, Gaming, Streaming, Family, Business, Student, Traveler):");
+        Console.Write("Masukkan nama paket (Basic, Standard, Premium, Unlimited, Gaming, Streaming, Family, Business, Student, Traveler):");
         string namaPaket = Console.ReadLine();
 
         // Get KodePaket berdasarkan nama paket
         string kodepaket = kode.getKodePaket(namaPaket);
 
         Console.WriteLine("Kode paket untuk " + namaPaket + " adalah: " + kodepaket);
+
+        Console.WriteLine("------------");
+
+        // Membuat objek MesinKopi
+        MesinKopi mesin = new MesinKopi();
+
+        Console.WriteLine("Simulasi State Kopi");
+        Console.WriteLine("");
+
+        mesin.PowerOn(); // OFF -> STANDBY
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        mesin.StartBrew(); // STANDY -> BREWING
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        mesin.FinishBrew(); // BREWING -> STANDBY
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        mesin.StartMaintenance(); // STANDBY -> MAINTENANCE
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        mesin.FinishMaintenance(); // MAINTENANCE -> STANDBY
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        mesin.PowerOff(); // STANDBY -> OFF
+        mesin.TampilkanState(); // Tampilkan state saat ini
+        Console.WriteLine("");
+
+        // Simulasi perubahan state yang tidak valid
+        mesin.FinishMaintenance(); // MAINTENANCE -> STANDBY (tidak valid karena mesin sedang OFF)
     }
 }
